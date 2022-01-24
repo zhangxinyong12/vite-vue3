986121a2cd7d983522d609b4fc7f6b5723c2a1a2
张新永 <xinyong.zhang@zen-x.com.cn>
2022-01-24 11:46:00 +0800

    feat: vite vue3

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>n:{{ n }}</h3>
+    <button @click="add">add</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const n = ref(1)
+const add = () => {
+  n.value++
+}
+</script>
